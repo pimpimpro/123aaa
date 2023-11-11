79e3b6f9a52e201cc7b6c5fbeb8b86dfd1762b5c
pimpimpro <quang123hyhy@gmail.com>
2023-11-11 16:04:18 +0700

    add login va register

--- view/header.php
+++ view/header.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Run Elite</title>
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/login.css">
     <link rel="icon" href="image/runelite-removebg.png">
     <link rel="stylesheet" href="fontawesome-free-6.4.2-web/fontawesome-free-6.4.2-web/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Lemon&display=swap" rel="stylesheet">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>                
 
 </head>
     <!-- BIGIN HEADER -->
---
+++ view/register.php
@@ -0,0 +1,40 @@
+
+<section class="container-login forms"> 
+                <div class="form login">      
+                    <div class="form-content">
+                        <h2>Welcom to</h2>
+                        <h1>Run ELite</h1>
+                        <form action="#">
+                            <div class="field input-field">
+                                <label for="">Username</label><br>
+                                <input type="text" placeholder="Username" class="input" >
+                            </div>
+    
+                            <div class="field input-field">
+                                <label for="password">Password</label> <br>
+                                <input type="password" placeholder="Password" >     
+                            </div>
+                            <div class="field input-field">
+                                <label for="password"> Nhập lại Password</label> <br>
+                                <input type="password" placeholder=" Nhập lại Password" >     
+                            </div>
+    
+                            <div class="form-link">
+                                <input type="checkbox" id="showPassword"> Show Password
+                                <a href="#" class="forgot-pass">Forgot password?</a>
+                            </div>
+    
+                            <div class="button-login">
+                                <button>Register</button>
+                            </div>
+                        </form>
+                        <div class="form-register">
+                            <span>Don't have an account? <a href="#" class="link signup-link">Signup</a></span>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="login-image">
+                    <img src="image/l.jpg" alt="">
+                </div>        
+        </section>
